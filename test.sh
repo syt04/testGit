788b9b1bba5cec0367cca2578bba43f7c407f1ff
syt1 <soyatuewv123@gmail.com>
2022-06-08 21:01:36 +0900

    ad

--- test.sh
+++ test.sh
@@ -1,7 +1,2 @@
 git merge ${1}
 
-<<<<<<< HEAD
-sleep 10
-
-=======
->>>>>>> feature/Test/Test_6
--- test2.sh
+++ test2.sh
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-./test.sh ${1}
-if [ $? =0 ] ; then
-echo "マージ成功"
-
-else 
-
-echo "失敗"
-
-fi
-
-sleep 10
-=======
 ./test.sh ${1} 
 if [ $? = 0 ] ; then
 echo "マージ成功"
@@ -21,4 +8,3 @@ git merge --abort
 fi
 
 sleep 5
->>>>>>> feature/Test/Test_6
